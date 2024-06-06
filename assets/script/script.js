9576345363af18a79bac9b8fbe08dd91b539699b
usama usman <usmanusama4811@gmail.com>
2024-06-06 12:02:57 +0500

    meting Changes done

---
+++ assets/script/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.share-button').click(function() {
+        $('.share-button').removeClass('active'); // Remove active class from all buttons
+        $(this).addClass('active'); // Add active class to the clicked button
+    });
+
+    
+});
+
+
+
